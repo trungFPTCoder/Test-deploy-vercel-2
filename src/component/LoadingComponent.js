d2ffa4ed6b548c0abd6f93efd1ca6dd31f3c47e4
Nguyễn Ngọc Trung <trungnnde180575@fpt.edu.vn>
2024-12-06 22:34:09 +0700

    Update LoadingComponent.js

--- src/component/LoadingComponent.js
+++ src/component/LoadingComponent.js
@@ -1,10 +1,11 @@
 export const LoadingComponent = () => {
 	return (
-		<>
+		<div className="bg-dark" style={{padding:'300px'}}>
 			<div className="d-flex align-items-center justify-content-center h-100">
-				<div className="spinner-border text-primary" role="status" />
+				<div className="spinner-border text-primary" style={{ width: '4rem', height: '4rem' }} role="status" />
 			</div>
-		</>
+			<p className="text-center h5 mt-3 text-light">Đang tải...</p>
+		</div>
 	);
 };
 
