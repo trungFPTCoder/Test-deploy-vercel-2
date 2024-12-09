a05ce0fa3afcd0c492f0de42c44f775a4dba86f7
Nguyễn Ngọc Trung <trungnnde180575@fpt.edu.vn>
2024-12-10 00:41:16 +0700

    Update WatchMovie.js

--- src/component/WatchMovie.js
+++ src/component/WatchMovie.js
@@ -115,7 +115,7 @@ function WatchMovie() {
                                             <button
                                                 className='btn'
                                                 onClick={() => {
-                                                    const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=https://test-deploy-vercel-2.vercel.app/watch/${slug}`;
+                                                    const shareUrl = `https://www.facebook.com/sharer/sharer.php?u=https://mycinemavn.vercel.app/watch/${slug}`;
                                                     const popupWidth = 600;
                                                     const popupHeight = 400;
 
@@ -138,7 +138,7 @@ function WatchMovie() {
                                             <button
                                                 className='btn'
                                                 onClick={() => {
-                                                    const shareUrl = `https://twitter.com/intent/tweet?url=https://test-deploy-vercel-2.vercel.app/watch/${slug}`;
+                                                    const shareUrl = `https://twitter.com/intent/tweet?url=https://mycinemavn.vercel.app/watch/${slug}`;
                                                     const popupWidth = 600;
                                                     const popupHeight = 400;
 
