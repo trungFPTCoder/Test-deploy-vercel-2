27fc15ba7eb2e6d78965a64aba798b26390dee39
Nguyễn Ngọc Trung <trungnnde180575@fpt.edu.vn>
2024-12-20 21:30:21 +0700

    Update SearchMovie.js

--- src/component/SearchMovie.js
+++ src/component/SearchMovie.js
@@ -77,7 +77,7 @@ function SearchMovie() {
                         <div className='row'>
                             {searchMovies.items.map((movie) => (
                                 <div className='col-6 col-md-2 col-sm-4 mb-3'>
-                                    <div className='card position-relative tooltip-wrapper border-0 w-100' >
+                                    <div className='card position-relative tooltip-wrapper border-0 w-100 movie--width' >
                                         <div className='img-container position-relative overflow-hidden'>
                                             <img
                                                 src={movie.thumb_url}
