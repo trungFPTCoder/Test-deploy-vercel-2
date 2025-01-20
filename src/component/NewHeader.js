66f0f38dc06cd37627e1b25362d1a6af0aed0f68
Nguyễn Ngọc Trung <trungnnde180575@fpt.edu.vn>
2025-01-20 15:41:08 +0700

    Update NewHeader.js

--- src/component/NewHeader.js
+++ src/component/NewHeader.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import '../assest/NewNavbar.css'
 function NewHeader() {
-    const [navbarBg, setNavbarBg] = useState('bg-dark opacity-75');
+    const [navbarBg, setNavbarBg] = useState('bg-dark opacity-75 pt-2 pb-2');
     const [searchKeyword, setSearchKeyword] = useState('');
     const [searchInput, setSearchInput] = useState('d-none');
     const [isSearchActive, setIsSearchActive] = useState(false);
